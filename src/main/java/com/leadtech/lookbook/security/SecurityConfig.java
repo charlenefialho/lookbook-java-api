dea1d8f7a0c0505cede3149c3a10db44478880ef
charlenefialho <fialhocharlene@gmail.com>
2024-09-29 12:53:38 -0300

    feat: added users and roles

--- src/main/java/com/leadtech/lookbook/security/SecurityConfig.java
+++ src/main/java/com/leadtech/lookbook/security/SecurityConfig.java
@@ -3,31 +3,56 @@ package com.leadtech.lookbook.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-	@Bean
+	
+	 @Bean
+	    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+	        UserDetails admin = User.withUsername("admin")
+	            .password(passwordEncoder.encode("admin123"))
+	            .roles("ADMIN")
+	            .build();
+
+	        UserDetails user = User.withUsername("user")
+	            .password(passwordEncoder.encode("user123"))
+	            .roles("USER")
+	            .build();
+
+	        return new InMemoryUserDetailsManager(admin, user);
+	    }
+	 
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests((requests) -> requests
-        		.requestMatchers("/login", "/logout", "/**?lang=*").permitAll()
-                .anyRequest().authenticated() 
+                .requestMatchers("/login", "/logout", "/**?lang=*").permitAll() 
+                .requestMatchers("/monitor/**").hasRole("ADMIN") 
+                .anyRequest().authenticated()
             )
             .formLogin((form) -> form
                 .loginPage("/login")
-                .defaultSuccessUrl("/clothing") 
-                .failureUrl("/login?falha=true") 
-                .permitAll()) 
+                .defaultSuccessUrl("/clothing")
+                .failureUrl("/login?falha=true")
+                .permitAll())
             .logout((logout) -> logout
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout=true") 
-                .permitAll()); 
+                .logoutSuccessUrl("/login?logout=true")
+                .permitAll());
 
         return http.build();
     }
-	
-	
 }
+
